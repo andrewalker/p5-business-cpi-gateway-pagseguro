c37330af11f3350c1e3f2a37fbe8b1fa9db6c991
André Walker <andre@andrewalker.net>
2013-02-20 20:29:09 -0300

    Implement first version of get_checkout_code

--- lib/Business/CPI/Gateway/PagSeguro.pm
+++ lib/Business/CPI/Gateway/PagSeguro.pm
@@ -4,7 +4,7 @@ package Business::CPI::Gateway::PagSeguro;
 use Moo;
 use XML::LibXML;
 use Carp;
-use LWP::Simple ();
+use LWP::UserAgent;
 use URI;
 use URI::QueryParam;
 use DateTime;
@@ -160,7 +160,7 @@ sub _load_xml_from_url {
     my ($self, $url) = @_;
 
     return XML::LibXML->load_xml(
-        string => LWP::Simple::get( $url )
+        string => LWP::UserAgent->new->get( $url )->decoded_content
     )->firstChild();
 }
 
@@ -281,6 +281,145 @@ sub get_hidden_inputs {
         $self->_get_hidden_inputs_for_items($info->{items}),
         $self->_get_hidden_inputs_for_cart($info->{cart}),
     );
+}
+
+sub get_checkout_code {
+    my ($self, $info) = @_;
+
+    # XXX: should the encoding be parameterized?
+    my $doc = XML::LibXML::Document->new( '1.0', 'UTF-8' );
+    $doc->setStandalone(1);
+
+    my $root = $doc->createElement('checkout');
+
+    my $currency = $doc->createElement('currency');
+    $currency->appendText($self->currency);
+
+    my $payment_id = $doc->createElement('reference');
+    $payment_id->appendText($info->{payment_id});
+
+    my $sender = $doc->createElement('sender');
+
+    my $sender_name = $doc->createElement('name');
+    $sender_name->appendText($info->{buyer}->name);
+
+    my $sender_email = $doc->createElement('email');
+    $sender_email->appendText($info->{buyer}->email);
+
+    $sender->appendChild($sender_name);
+    $sender->appendChild($sender_email);
+
+    my $shipping;
+    my $cart = $info->{cart};
+    if ($cart->address_street) {
+        $shipping = $doc->createElement('shipping');
+        my $address = $doc->createElement('address');
+
+        my $street = $doc->createElement('street');
+        $street->appendText($cart->address_street);
+
+        my $number = $doc->createElement('number');
+        $number->appendText($cart->address_number);
+
+        my $complement = $doc->createElement('complement');
+        $complement->appendText($cart->address_complement);
+
+        my $district = $doc->createElement('district');
+        $district->appendText($cart->address_district);
+
+        my $postal_code = $doc->createElement('postalCode');
+        $postal_code->appendText($cart->address_zip_code);
+
+        my $city = $doc->createElement('city');
+        $city->appendText($cart->address_city);
+
+        my $state = $doc->createElement('state');
+        $state->appendText($cart->address_state);
+
+        my $country = $doc->createElement('country');
+        $country->appendText(
+            uc( Locale::Country::country_code2code( $cart->address_country ) )
+        );
+
+        $address->appendChild($street);
+        $address->appendChild($number);
+        $address->appendChild($complement);
+        $address->appendChild($district);
+        $address->appendChild($postal_code);
+        $address->appendChild($city);
+        $address->appendChild($state);
+        $address->appendChild($country);
+
+        $shipping->appendChild($address);
+    }
+
+    my $handling = $cart->handling || 0;
+    my $discount = $cart->discount || 0;
+    my $tax      = $cart->tax      || 0;
+
+    my $extra_amount = $tax + $handling - $discount;
+    my $ea;
+
+    if ($extra_amount) {
+        $ea = $doc->createElement('extraAmount');
+        $ea->appendText($extra_amount);
+    }
+
+    my $items = $doc->createElement('items');
+
+    for my $i (@{ $info->{items} }) {
+        my $item = $doc->createElement('item');
+
+        my $id = $doc->createElement('id');
+        $id->appendText($i->id);
+
+        my $desc = $doc->createElement('description');
+        $desc->appendText($i->description);
+
+        my $amount = $doc->createElement('amount');
+        $amount->appendText($i->price);
+
+        my $qty = $doc->createElement('quantity');
+        $qty->appendText($i->quantity);
+
+        my $weight = $doc->createElement('weight');
+        $weight->appendText($i->weight) if $i->weight;
+
+        my $ship = $doc->createElement('shippingCost');
+        $ship->appendText($i->shipping) if $i->shipping;
+
+        $item->appendChild($id);
+        $item->appendChild($desc);
+        $item->appendChild($amount);
+        $item->appendChild($qty);
+        $item->appendChild($weight) if $i->weight;
+        $item->appendChild($ship) if $i->ship;
+
+        $items->appendChild($item);
+    }
+
+    $root->appendChild($currency);
+    $root->appendChild($items);
+    $root->appendChild($payment_id);
+    $root->appendChild($sender);
+    $root->appendChild($shipping) if $shipping;
+    $root->appendChild($ea) if $ea;
+
+    $doc->setDocumentElement($root);
+
+    my $ua = LWP::UserAgent->new;
+
+    my $req = $ua->post(
+        $self->base_url . '/checkout',
+        Content_Type => 'application/xml; charset=UTF-8',
+        Content      => $doc->asString
+    );
+
+    die $req->status_line unless $req->is_success;
+
+    return XML::LibXML->load_xml(
+        string => $req->decoded_content
+    )->firstChild()->getChildrenByTagName('code')->string_value;
 }
 
 1;
